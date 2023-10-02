7c130f9b88c51dcd1a102a1ef4b1bd30675cc09a
LARA MICAELA FARINA <47871657@ort>
2023-10-02 08:24:38 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP09-Login MVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
