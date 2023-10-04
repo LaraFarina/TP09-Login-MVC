f971be9a285e5eb4cd5c551266fd8e558f342a6f
LARA MICAELA FARINA <47871657@ort>
2023-10-04 08:11:44 -0300

    View Registro Hecha

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function validarContraseña() {
+    var contraseña = document.getElementById("contrasena").value;
+    var confirmarContraseña = document.getElementById("confirmar-contrasena").value;
+
+    if (contraseña !== confirmarContraseña) {
+        alert("Las contraseñas no coinciden.");
+        return false;
+    }
+
+    // Verificar que la contraseña cumpla con los requisitos
+    var caracteres = /^(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    if (!caracteres.test(contraseña)) {
+        alert("La contraseña debe tener al menos 8 caracteres, una letra en mayúscula y un carácter especial.");
+        return false;
+    }
+
+    return true;
+}
