4a81b96d738e7616a1164053e2013b74a57e7787
LARA MICAELA FARINA <47871657@ort>
2023-10-04 09:05:24 -0300

    salta error en view model

--- Models/BD.cs
+++ Models/BD.cs
@@ -2,13 +2,13 @@ using System.Data.SqlClient;
 using Dapper; 
 
 public class BD{
-    private static string _connectionString = @"Server=.;A-PHZ2-CIDI-048;Trusted_Connection=True;";
+    private static string _connectionString = @"Server=.;Trusted_Connection=True;";
     private static Usuario _UserLog = null;
 
     public static void RegistrarUsuario(Usuario user){
-        string sql = "INSERT INTO Usuario(id, UserName, Contraseña, Email, Telefono, Genero) VALUES (@pid, @pUserName, @pContraseña, @pEmail, @pTelefono, @pGenero)";
+        string sql = "INSERT INTO Usuario(id, UserName, Contraseña, Nombre, Email, Telefono, ) VALUES (@pid, @pUserName, @pContraseña, @pNombre, @pEmail, @pTelefono, )";
         using(SqlConnection db = new SqlConnection(_connectionString)){
-            db.Execute(sql, new {pid = user.id, pUserName = user.UserName, pContraseña = user.Contraseña, pEmail = user.Email, pTelefono = user.Telefono, pGenero = user.Genero});
+            db.Execute(sql, new {pid = user.id, pUserName = user.UserName, pContraseña = user.Contraseña, pEmail = user.Email, pTelefono = user.Telefono, pNombre = user.Nombre});
         }
     }
 
@@ -28,10 +28,10 @@ public class BD{
     public static void CrearUsuario(Usuario User)
     {
         int RegistrosAñadidos = 0;
-        string sql = "INSERT INTO Usuario(UserName, Contraseña, Email, Telefono, Genero) VALUES (@pName, @pContra, @pMail, @pTel, @pGen)";
+        string sql = "INSERT INTO Usuario(UserName, Contraseña, Email, Nombre, Genero) VALUES (@pUsername, @pContra, @pMail, @pTel, @pNombre)";
         using(SqlConnection db = new SqlConnection(_connectionString))
         {
-            RegistrosAñadidos = db.Execute(sql, new {pName = User.UserName, pContra = User.Contraseña, pMail = User.Email, pTel = User.Telefono, pGen = User.Genero});
+            RegistrosAñadidos = db.Execute(sql, new {pUserName = User.UserName, pContra = User.Contraseña, pMail = User.Email, pTel = User.Telefono, pNombre = User.Nombre});
         }
         bool esValido = ValidacionUsuario(User.UserName, User.Contraseña);
     }
@@ -45,7 +45,4 @@ public class BD{
         }
         return(_UserLog != null);
     }
-
-
-
 }
